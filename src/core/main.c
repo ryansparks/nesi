e0dd14806c8c704b42f7cc036a5848c8bc2b3647
ryansparks <ryan@ryan-sparks.com>
2014-06-01 21:26:45 -0600

    initial commit

--- src/core/main.c
+++ src/core/main.c
@@ -5,50 +5,25 @@
 
 Boolean getPicture(void);
 
+int numPressed = 0;
+
 int main(void)
 {
     nesi.init();
-
-    camera.init();
-    dateTime.init();
-
-    DateAndTime tempTime;
-
-    tempTime.hour = 18;
-    tempTime.minute = 44;
-    tempTime.second = 0;
-
-    tempTime.month = MAY;
-    tempTime.day = 25;
-    tempTime.year = 14;
-
-    dateTime.set(tempTime);
+    usb.init();
+    usb.attach();
 
     while(1)
     {
         if(button.getStroke() == KEYSTROKE_SINGLE)
         {
             button.disableStroke();
-            ledB.dutycycle(1);
-            getPicture();
-            ledB.dutycycle(0);
+            numPressed++;
+            char toprint[32] = {0};
+            sprintf(toprint, "button pressed %c times", numPressed);
+            usb.print(toprint);
             button.enableStroke();
         }
     }
     return 0;
-}
-
-Boolean getPicture()
-{
-    char filename[32] = {0};
-    sprintf(filename, "%s.jpg", dateTime.getStamp());
-
-    char *temp = filename;
-    while(*temp)
-    {
-        if(*temp == ':')
-            *temp = '.';
-        ++temp;
-    }
-    return camera.getPix(filename);
 }
